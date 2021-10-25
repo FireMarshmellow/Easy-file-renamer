cab8ec6deb6afbdf4e739b13bc6b67135e881238
Mellow <62353091+FireMarshmallow@users.noreply.github.com>
2021-10-25 08:58:01 +0100

    made a GUI

--- main.py
+++ main.py
@@ -6,14 +6,55 @@ Code by
  | |  | |  __/ | | (_) \ V  V /  |  _| | | | |  __/
  |_|  |_|\___|_|_|\___/ \_/\_/   |_|   |_|_|  \___|
 check out my stuff on https://linktr.ee/Mellowfire
+i make ugly code but it works :)
 """
 
 import os, sys
-from typing import NamedTuple
+from typing import NamedTuple, Text
+import tkinter
+from tkinter import Tk, filedialog
 
-Path_to_folder = input("path to folder:")
-name_of_file = input("name:")
-number = int(input("Number to start:"))
+root = tkinter.Tk()
+root.title("Easy File Renamer")
+
+
+name_entry = tkinter.Entry(root)
+name_entry.insert(0, "")
+
+number_entry = tkinter.Entry(root)
+number_entry.insert(0, "")
+
+
+def start():
+    print(Path_to_folder)
+    hellow = str(name_entry.get())
+    hello = int(number_entry.get())
+    rename_files(Path_to_folder, hellow, hello)
+
+
+start_button = tkinter.Button(
+    root,
+    text="Start",
+    command=start,
+)
+
+
+def Path_to_folder():
+    global Path_to_folder
+    Path_to_folder = filedialog.askdirectory()
+    change_sorter_in(Path_to_folder)
+
+
+def change_sorter_in(Path_to_folder):
+    sorter_in_path["text"] = Path_to_folder
+
+
+sorter_in_path = tkinter.Button(
+    root, height=3, width=50, text="path to folder:", command=Path_to_folder
+)
+
+name_label = tkinter.Label(root, text="Name:")
+Num_label = tkinter.Label(root, text="Number to start:")
 
 
 def rename_files(path, name, num):
@@ -26,4 +67,12 @@ def rename_files(path, name, num):
         )
 
 
-rename_files(Path_to_folder, name_of_file, number)
+sorter_in_path.grid(row=0, column=0, columnspan=2)
+name_entry.grid(row=1, column=1)
+name_label.grid(row=1, column=0)
+
+number_entry.grid(row=2, column=1)
+Num_label.grid(row=2, column=0)
+
+start_button.grid(row=100, column=0, columnspan=2)
+root.mainloop()
